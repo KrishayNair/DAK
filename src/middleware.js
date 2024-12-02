5cd5968ce0854baaa771b0ac57bf4a4c806237cc
aadish03 <aadishgotekar03@gmail.com>
2024-12-03 01:51:33 +0530

    fix middleware to use it freely

--- src/middleware.js
+++ src/middleware.js
@@ -1,11 +1,15 @@
-import { verifySession } from "@/lib/auth";
+// import { verifySession } from "@/lib/auth";
 
 export async function middleware(request) {
-  return await verifySession(request);
+  //   return await verifySession(request);
+  console.log("yoyoyo");
 }
 
+// export const config = {
+//   matcher: [
+//     "/((?!api|_next/static|_next/image|.*\\.png$|login|signup|forgot-password).*)",
+//   ],
+// };
 export const config = {
-  matcher: [
-    "/((?!api|_next/static|_next/image|.*\\.png$|login|signup|forgot-password).*)",
-  ],
+  matcher: ["/(.*)"],
 };
