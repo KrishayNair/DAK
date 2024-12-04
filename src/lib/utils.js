7a6f12b3867ced435b80e9967d70dc009b1fcb0d
Krishay Nair <krishay958@gmail.com>
2024-12-04 14:24:37 +0530

    added function in utils and removed  utils.ts

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -4,3 +4,12 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs) {
   return twMerge(clsx(inputs))
 }
+
+export function buildImageUrl(path) {
+  if (path.startsWith("http")) {
+    return path;
+  }
+  
+  const base = process.env.NEXT_PUBLIC_API_URL + "media/";
+  return base + path;
+}
