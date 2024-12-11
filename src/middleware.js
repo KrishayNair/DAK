eaa42099963890e799f8c38447c3a48bdc232a24
NandaniKadave <nandani.kadave@somaiya.edu>
2024-12-11 11:53:58 +0530

    added logo in footer

--- src/middleware.js
+++ src/middleware.js
@@ -2,13 +2,13 @@ import { verifySession } from "@/lib/auth";
 
 export async function middleware(request) {
 
-  // return await verifySession(request);
-  console.log("yoyoyo");
+  return await verifySession(request);
+  // console.log("yoyoyo");
 
 }
 
-// export const config = {
-//   matcher: [
-//     "/((?!api|_next/static|_next/image|.*\\.png$|login|signup|forgot-password).*)",
-//   ],
-// };
+export const config = {
+  matcher: [
+    "/((?!api|_next/static|_next/image|.*\\.png$|login|signup|forgot-password).*)",
+  ],
+};
